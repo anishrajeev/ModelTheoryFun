7ffc5d48318ee83ec43cb8f85f777c83a7590484
Anishrajeev210 <37055371+Anishrajeev210@users.noreply.github.com>
2025-11-07 16:25:06 -0500

    I think im doing this wrong

--- ModelTheoryFun/Basic.lean
+++ ModelTheoryFun/Basic.lean
@@ -1,6 +1,8 @@
+import Mathlib.Data.Rat.Cast.Order
 import Mathlib.ModelTheory.Basic
 import Mathlib.ModelTheory.Syntax
 import Mathlib.ModelTheory.Semantics
+-- import Mathlib.Data.Rat.Order
 
 -- Lets define some cool models and theories
 open FirstOrder
@@ -55,8 +57,6 @@ No endpoint: ∀ x. ∃ y. x < y
 No endpoint: ∀ x. ∃ y. y < x
 Density: ∀ x. ∀ y. ∃ z. (x < z) ∧ (z < y)
 -/
-def conjunction (x : language.Sentence) (y : language.Sentence) : language.Sentence := ∼ (x ⟹ ∼ y)
-def disjunction (x : language.Sentence) (y : language.Sentence) : language.Sentence := (∼ x) ⟹ y
 
 def antireflexive : language.Sentence :=
 all (imp (rel rel_symbols.lt (fun x => var (Sum.inr (Fin.mk 0 (by simp))))) falsum)
@@ -115,7 +115,7 @@ def DLO_Theory : language.Theory := {antireflexive,
 instance : language.Structure Rat := rationalStructure
 instance : language.Structure ℕ := natStructure
 
-theorem ratAntiReflexive : antireflexive.Realize Rat := by
+theorem ratantiReflexive : antireflexive.Realize Rat := by
   dsimp [Sentence.Realize, antireflexive, Formula.Realize]
   intros x xltx
   dsimp [BoundedFormula.Realize]
@@ -123,6 +123,38 @@ theorem ratAntiReflexive : antireflexive.Realize Rat := by
   unfold Structure.RelMap at xltx
   dsimp [instStructureLanguageRat] at xltx
   exact Rat.lt_irrefl xltx
-
+theorem ratTransitivity : transitivity.Realize Rat := by
+  rw [transitivity]
+  rw [Sentence.Realize]
+  intro x y z
+  rw [realize_imp]
+  intro h
+  rw [realize_not] at h
+  rw [realize_imp] at h
+  simp at h
+  cases h with
+  |intro hx hy =>
+  have hxy : x < y := hx
+  have hyz : y < z := hy
+  exact lt_trans hxy hyz
+theorem ratLinearOrder : linear_order.Realize Rat := by
+  rw [linear_order]
+  rw [Sentence.Realize]
+  rw [Formula.Realize]
+  intro x y
+  rw [realize_imp]
+  intro h₁
+  rw [realize_imp]
+  intro h₂
+  rw [realize_bdEqual]
+  rw [realize_not] at h₁ h₂
+  have hxy : ¬ (x < y) := h₁
+  have hyx : ¬ (y < x) := h₂
+  cases lt_trichotomy x y with
+  | inl hc => exact (False.elim (hxy hc))
+  | inr smallhypothesis =>
+    cases smallhypothesis with
+    | inl ans => exact ans
+    | inr hc => exact (False.elim (hyx hc))
 
 end OrderedLang
