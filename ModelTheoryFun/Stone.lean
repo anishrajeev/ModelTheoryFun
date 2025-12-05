e9df9d11386ca94af8c8c602b48e310a219e6edf
Anishrajeev210 <37055371+Anishrajeev210@users.noreply.github.com>
2025-12-05 18:15:16 -0500

    Format

--- ModelTheoryFun/Stone.lean
+++ ModelTheoryFun/Stone.lean
@@ -16,12 +16,13 @@ namespace TypeSpace
 variable {L : FirstOrder.Language} {T : L.Theory}
 
 def basis {n : ℕ} : Set (Set (T.CompleteType (Fin n))) := {X | ∃ φ, X = {p | φ ∈ p}}
-def basicOpen {n : ℕ} (φ : L[[Fin n]].Sentence) : Set (T.CompleteType (Fin n)) :=
+def BasicOpen {n : ℕ} (φ : L[[Fin n]].Sentence) : Set (T.CompleteType (Fin n)) :=
   {X : T.CompleteType (Fin n) | φ ∈ X}
 
 instance (n : ℕ) : TopologicalSpace (T.CompleteType (Fin n)) :=
     TopologicalSpace.generateFrom (basis)
-lemma exists_separating_sets {n : ℕ} (x y : T.CompleteType (Fin n))
+
+lemma ExistsSeperatingSets {n : ℕ} (x y : T.CompleteType (Fin n))
                          (φ : L[[Fin n]].Sentence)
                          (φ_inx_φ_niny : φ ∈ x.toTheory ∧ φ ∉ y.toTheory) :
                          ∃ u v : Set (T.CompleteType (Fin n)), IsOpen u ∧ IsOpen v ∧
@@ -44,6 +45,9 @@ lemma exists_separating_sets {n : ℕ} (x y : T.CompleteType (Fin n))
                               intro Γ
                               contrapose!
                               exact (Γ.not_mem_iff φ).1
+lemma basic_open_compl {n : ℕ} (T : L.Theory) (φ : L[[Fin n]].Sentence) :
+  BasicOpen (T := T) φ.not = (BasicOpen φ)ᶜ := by ext p ; exact p.not_mem_iff φ
+
 instance {n : ℕ} : T2Space (T.CompleteType (Fin n)) := by
   constructor
   intro x y xneqy
@@ -58,7 +62,7 @@ instance {n : ℕ} : T2Space (T.CompleteType (Fin n)) := by
   · rw [Set.subset_def] at x_nsub_y
     push_neg at x_nsub_y
     rcases x_nsub_y with ⟨φ, φ_x_y⟩
-    exact exists_separating_sets x y φ φ_x_y
+    exact ExistsSeperatingSets x y φ φ_x_y
   · rw [Set.subset_def] at y_nsub_x
     push_neg at y_nsub_x
     rcases y_nsub_x with ⟨φ, φ_x_y⟩
@@ -67,21 +71,18 @@ instance {n : ℕ} : T2Space (T.CompleteType (Fin n)) := by
       · exact (x.not_mem_iff φ).2 φ_x_y.2
       · intro nφy
         exact (y.not_mem_iff φ).1 nφy φ_x_y.1
-    exact exists_separating_sets x y φ.not swapped
-
-lemma basicOpen_compl {n : ℕ} (T : L.Theory) (φ : L[[Fin n]].Sentence) :
-  basicOpen (T := T) φ.not = (basicOpen φ)ᶜ := by ext p ; exact p.not_mem_iff φ
+    exact ExistsSeperatingSets x y φ.not swapped
 
 instance {n : ℕ} : CompactSpace (T.CompleteType (Fin n)) := by
   constructor
   rw[isCompact_iff_ultrafilter_le_nhds]
   intros F hf_ultra
-  let pTheory : L[[Fin n]].Theory := {φ | basicOpen φ ∈ F}
+  let pTheory : L[[Fin n]].Theory := {φ | BasicOpen φ ∈ F}
   have p_isConsistentWith : (L.lhomWithConstants (Fin n)).onTheory T ⊆ pTheory := by
     intro φ φinT
-    have uni : basicOpen (T := T) φ = Set.univ := by
+    have uni : BasicOpen (T := T) φ = Set.univ := by
       ext h
-      simp [basicOpen, Set.mem_setOf_eq, Set.mem_univ, iff_true]
+      simp [BasicOpen, Set.mem_setOf_eq, Set.mem_univ, iff_true]
       exact h.subset φinT
     rw[Set.mem_setOf_eq, uni]
     exact Filter.univ_mem
@@ -90,7 +91,7 @@ instance {n : ℕ} : CompactSpace (T.CompleteType (Fin n)) := by
     constructor
     · rw[Theory.isSatisfiable_iff_isFinitelySatisfiable, Theory.IsFinitelySatisfiable]
       intro φᵢ h_subset
-      have in_uf : ∀ φ ∈ φᵢ, basicOpen φ ∈ F.toFilter := by
+      have in_uf : ∀ φ ∈ φᵢ, BasicOpen φ ∈ F.toFilter := by
         intro φ φ_in_finset
         exact h_subset φ_in_finset
       rw[←Filter.biInter_finset_mem φᵢ] at in_uf
@@ -98,9 +99,9 @@ instance {n : ℕ} : CompactSpace (T.CompleteType (Fin n)) := by
       have subset : SetLike.coe φᵢ ⊆ T.toTheory := by rwa[Set.mem_iInter₂] at T_inter
       exact Theory.IsSatisfiable.mono T.isMaximal'.1 subset
     · intro φ
-      rcases Ultrafilter.mem_or_compl_mem F (basicOpen φ) with  φ_in | φ_nin
+      rcases Ultrafilter.mem_or_compl_mem F (BasicOpen φ) with  φ_in | φ_nin
       · exact Or.inl φ_in
-      · rw[←basicOpen_compl T φ] at φ_nin
+      · rw[←basic_open_compl T φ] at φ_nin
         exact Or.inr φ_nin
   let p : T.CompleteType (Fin n) := ⟨pTheory, p_isConsistentWith, p_isMaximal⟩
   use p
